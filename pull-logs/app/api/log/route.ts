663f7ec47bc603afecec98755ac227e2c80eb68e
surendrakumar6350 <kamleshksks456@gmail.com>
2025-05-05 04:35:08 -0700

    fix: Changed logs fetching query to sort by MongoDB ObjectId (_id) instead of timestamp

--- pull-logs/app/api/log/route.ts
+++ pull-logs/app/api/log/route.ts
@@ -2,30 +2,21 @@ import { connectDb } from "@/dbConnection/connect";
 import { Log } from "@/dbConnection/Schema/logSchema";
 import { NextResponse } from "next/server";
 
-type LogType = {
-    _id: string;
-    user: string;
-    values: string;
-    page: string;
-    ip: string;
-    timestamp: string;
-    date: string;
-};
-
 export const dynamic = "force-dynamic";
+
 export async function GET(request: Request): Promise<NextResponse> {
     try {
         await connectDb();
-        const recentLogs: LogType[] = await Log.find().sort({ timestamp: -1 }).limit(20);
+        const recentLogs = await Log.find().sort({ _id: -1 }).limit(20).lean();
 
         return NextResponse.json({
             success: true,
             message: recentLogs
-        }, { status: 200 })
+        }, { status: 200 });
     } catch (error) {
         return NextResponse.json({
             success: false,
             message: "Server Error"
-        })
+        }, { status: 500 });
     }
 }
