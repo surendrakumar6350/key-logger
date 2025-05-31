302a5bf8dd57f78cc84158d80200229a213be67d
surendrakumar6350 <kamleshksks456@gmail.com>
2025-05-31 02:57:37 -0700

    update: verify jwt before fetching logs from database

--- pull-logs/app/api/log/route.ts
+++ pull-logs/app/api/log/route.ts
@@ -1,19 +1,36 @@
 import { connectDb } from "@/dbConnection/connect";
 import { Log } from "@/dbConnection/Schema/logSchema";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request): Promise<NextResponse> {
     try {
+        const cookieStore = await cookies();
+        const tokenCookie = cookieStore.get('token');
+
+        if (!tokenCookie?.value) {
+            return NextResponse.json({
+                success: false,
+                message: "Unauthorized",
+            }, { status: 401 });
+        }
+
+        const token = tokenCookie.value;
+        jwt.verify(token, process.env.JWT_SECRET!);
+
+
         await connectDb();
-        const recentLogs = await Log.find().sort({ _id: -1 }).limit(40).lean();
+        const recentLogs = await Log.find().sort({ _id: -1 }).limit(50).lean();
 
         return NextResponse.json({
             success: true,
             message: recentLogs
         }, { status: 200 });
     } catch (error) {
+        console.log(error);
         return NextResponse.json({
             success: false,
             message: "Server Error"
